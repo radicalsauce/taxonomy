d89d4a60273d8638d53fc52e3d32cb6b21228660
radicalsauce <krhale@gmail.com>
2014-09-15 20:28:01 -0700

    BeesBeesBeeeeeeees

--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,3 +1,20 @@
 var HoneyMakerBee = function() {
+  Bee.call(this);
+  this.age = 10;
+  this.job = "make honey";
+  this.honeyPot = 0;
+
 };
+
+HoneyMakerBee.prototype = Object.create(Grub.prototype);
+HoneyMakerBee.prototype.makeHoney = function () {
+  return this.honeyPot++;
+};
+HoneyMakerBee.prototype.giveHoney = function () {
+  return this.honeyPot--;
+};
+
+HoneyMakerBee.prototype.constructor = HoneyMakerBee;
+
+
 
--- src/RetiredForager.js
+++ src/RetiredForager.js
@@ -1,3 +1,21 @@
 var RetiredForagerBee = function() {
+  Grub.call(this);
+  ForagerBee.call(this);
+  this.age = 40;
+  this.job = "gamble";
+  this.canFly = false;
+  this.color = "grey";
+
 };
 
+RetiredForagerBee.prototype.eat = Grub.prototype.eat;
+
+RetiredForagerBee.prototype.forage = function(){
+  return "I am too old, let me play cards instead";
+};
+
+RetiredForagerBee.prototype.gamble = function(treasure) {
+  this.treasureChest.push(treasure);
+
+
+};
